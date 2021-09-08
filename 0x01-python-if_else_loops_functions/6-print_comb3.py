7c74280825a5fbf659a0971389f42fd368dc128c
ruby-alx <rattari.ae@gmail.com>
2021-09-08 02:00:43 +0000

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for x in range(0, 10):
-    for y in range(x + 1, 10):
-        if (not (x == 8 and y == 9)):
-            print("{}{}".format(x, y), end=", ")
-        else:
-            print("{}{}".format(x, y))
+for num in range(1, 89):
+    if num % 10 > num / 10:
+        print("{:02d}".format(num), end=', ')
+
+print("{:02d}".format(89))
