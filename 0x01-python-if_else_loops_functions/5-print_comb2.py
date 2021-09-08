0ba0c20dd0e93c6fa5b0a1e9a9ba0f1a952f73a8
ruby-alx <rattari.ae@gmail.com>
2021-09-08 02:52:07 +0000

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
+for int in range(0, 100):
+    if int != 99:
+        print('{:02d},'.format(int), end=' ')
     else:
-        print("{:02}".format(number), end=", ")
+        print('{}'.format(int))
