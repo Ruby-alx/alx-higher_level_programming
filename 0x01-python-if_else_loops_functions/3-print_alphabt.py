9604e2a59921a09f3a762d192e9ebfd0c441a26c
ruby-alx <rattari.ae@gmail.com>
2021-09-08 02:19:49 +0000

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print("{}".format(chr(i)), end='')
+for letter in range(97, 123):
+    if chr(letter) == 'q' or chr(letter) == 'e':
+        continue
+    print("{:s}".format(chr(letter)), end='')
