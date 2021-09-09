4d700f6e2033953f046e627f0e6354fbeff91c0e
ruby-alx <rattari.ae@gmail.com>
2021-09-09 15:38:23 +0000

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    i = 1
-    if len(argv) < 2:
-        print("0 arguments.")
-    elif len(argv) < 3:
-        print("1 argument:")
+    arguments = argv[1:]
+    elements = len(arguments)
+    if elements == 0:
+        print("{} arguments.".format(elements))
+    elif elements == 1:
+        print("{} argument:".format(elements))
+        print("{}: {}".format(elements, arguments[0]))
     else:
-        print("{:d} arguments:".format(len(argv) - 1))
-    for arg in argv[1:]:
-        print("{:d}: {:s}".format(i, arg))
-        i += 1
+        print("{} arguments:".format(elements))
+        for i in range(0, elements):
+            print("{}: {}".format(i + 1, arguments[i]))
