5d200232d33866ab2b9ef11ef6a36e4d57ebf262
ruby-alx <rattari.ae@gmail.com>
2021-09-13 13:51:46 +0000

    0x03-python-data_structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
-#r/bin/python3
+#!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
+    if idx > len(my_list) - 1 or idx < 0:
         return None
-    else:
-        return my_list[idx]
+    return my_list[idx]
