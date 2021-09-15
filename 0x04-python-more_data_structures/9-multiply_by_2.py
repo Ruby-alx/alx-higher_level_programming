047afa31f22e5e3485845bc38fa362ba3aeb0512
ruby-alx <rattari.ae@gmail.com>
2021-09-15 22:10:22 +0000

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if a_dictionary is None:
-        return
-    return dict(map(lambda item: (item[0], item[1] * 2), a_dictionary.items()))
+    """Return a new dictionary with all values multipled by 2."""
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
