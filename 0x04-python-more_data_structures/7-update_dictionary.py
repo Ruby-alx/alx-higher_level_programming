2e288ac4d5c11a8fd385f9af80ed79ecc885f74e
ruby-alx <rattari.ae@gmail.com>
2021-09-15 22:15:58 +0000

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is None:
-        return
-    a_dictionary[key] = value
-    return a_dictionary
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return (my_dict)
