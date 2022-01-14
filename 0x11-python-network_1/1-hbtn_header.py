12a0be8cb64e5c46133caf9c59258e8791d1d7ae
ruby-alx <rattari.ae@gmail.com>
2022-01-13 18:53:32 -0800

    0x11-python-network_1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""Fetches a header of a response from a URL."""
+""" print request id """
+import urllib.request
 import sys
-from urllib import request
 
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-        with request.urlopen(url) as response:
-            print(response.headers['X-Request-Id'])
-            
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        ri = response.getheader('X-Request-Id')
+        print(ri)
